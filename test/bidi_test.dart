f8956d488de739419dd0414040497f30079c37b2
Mahdi K. Fard <mkfard@outlook.com>
2022-11-02 16:38:30 +0330

    Adds some tests.

--- test/bidi_test.dart
+++ test/bidi_test.dart
@@ -1,3 +1,21 @@
-//import 'package:bidi/bidi.dart';
+import 'package:bidi/bidi.dart' as bidi;
+import 'package:test/test.dart';
 
-void main() {}
+void main() {
+  test('Decomposition Tests', () {
+    final a = String.fromCharCodes(bidi.logicalToVisual('أ'));
+    final b = String.fromCharCodes(bidi.logicalToVisual('إ'));
+    final c = String.fromCharCodes(bidi.logicalToVisual('ؤ'));
+
+    final d = String.fromCharCodes(bidi.logicalToVisual('خطأ'));
+    final e = String.fromCharCodes(bidi.logicalToVisual('كؤ'));
+    final f = String.fromCharCodes(bidi.logicalToVisual('مئ'));
+
+    expect(a, 'ﺃ');
+    expect(b, 'ﺇ');
+    expect(c, 'ﺅ');
+    expect(d, 'ﺄﻄﺧ');
+    expect(e, 'ﺆﻛ');
+    expect(f, 'ﺊﻣ');
+  });
+}
